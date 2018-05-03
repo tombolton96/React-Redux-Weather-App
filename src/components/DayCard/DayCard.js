5150395d6bb5b8b6ca7cae1bcafa5262c597177a
Tom Bolton <tbolton96@icloud.com>
2018-05-03 20:03:44 +0200

    current temp, day and date displayed

--- src/components/DayCard/DayCard.js
+++ src/components/DayCard/DayCard.js
@@ -14,8 +14,44 @@ class DayCard extends Component {
                 description: props.weather.description,
                 temperature: props.weather.temperature,
                 icon: props.weather.icon
-            }
+            },
+            date: this.getDate(props.date),
+            day: this.getDay(props.date)
         };
+    }
+
+    getDate(ms) {
+        return new Date(ms * 1000).toLocaleDateString();
+      }
+
+    getDay(ms) {
+        const day = new Date(ms * 1000).getDay();
+
+        switch(day) {
+            case 0:
+                return 'Sunday';
+                break;
+            case 1:
+                return 'Monday';
+                break;
+            case 2:
+                return 'Tuesday';
+                break;
+            case 3:
+                return 'Wednesday';
+                break;
+            case 4:
+                return 'Thursday';
+                break;
+            case 5:
+                return 'Friday';
+                break;
+            case 6:
+                return 'Saturday';
+                break;
+            default:
+                console.log('error loading date');
+        }
     }
 
     // componentWillReceiveProps(newProps) {
@@ -34,7 +70,9 @@ class DayCard extends Component {
     render() {
         return(
             <div className="container">
-                <p>{this.state.weather.temperature} &deg;C</p>
+                <h3>{this.state.day}</h3>
+                <p>{this.state.date}</p>
+                <p>{this.state.weather.temperature}&deg;C</p>
                 <img src={`http://openweathermap.org/img/w/${this.state.weather.icon}.png`} alt={this.state.weather.description}/>
                 <p>{this.state.weather.description}</p>
             </div>
