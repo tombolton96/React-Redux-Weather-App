7bc05971e70fa9903af302541896ed990ffb4a0c
tombolton96 <tbolton96@icloud.com>
2018-05-25 01:00:36 +0200

    indicator styling

--- src/components/Slider/Indicator.js
+++ src/components/Slider/Indicator.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './indicator.scss';
 
 class Indicator extends Component {
     render() {
         const { sliderCount } = this.props;
 
-        const style = {
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            margin: '20px 10px'
-        };
-        const selected = {
-            height: '1px',
-            width: '100%',
-            border: '1px solid rgba(255,255,255,1)',
-            backgroundColor: 'rgba(190,190,190,1)'
-        };
-        const notSelected = {
-            height: '1px',
-            width: '100%',
-            border: '1px solid rgba(190,190,190,0.3)',
-            backgroundColor: 'rgba(190,190,190,0.3)'
-        };
-
         return(
-            <div style={style}>
-                <div style={sliderCount === 0 ? selected : notSelected}></div>
-                <div style={sliderCount === 1 ? selected : notSelected}></div>
-                <div style={sliderCount === 2 ? selected : notSelected}></div>
-                <div style={sliderCount === 3 ? selected : notSelected}></div>
-                <div style={sliderCount === 4 ? selected : notSelected}></div>
+            <div className='indicator'>
+                <div className={sliderCount === 0 ? 'selected' : 'notselected'}></div>
+                <div className={sliderCount === 1 ? 'selected' : 'notselected'}></div>
+                <div className={sliderCount === 2 ? 'selected' : 'notselected'}></div>
+                <div className={sliderCount === 3 ? 'selected' : 'notselected'}></div>
+                <div className={sliderCount === 4 ? 'selected' : 'notselected'}></div>
             </div>
         );
     }
